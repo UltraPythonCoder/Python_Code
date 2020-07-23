cc4727c2cffc26fcc6ffe65a0390597afbab2807
UltraPythonCoder <64812511+UltraPythonCoder@users.noreply.github.com>
2020-07-22 17:14:36 -0700

    StringToListToDictionary
    Input: Python
    Output: {0 : 'P' , 1 : 'y', 2 : 't', 3 : 'h' , 4 : 'o' , 5 : 'n'}

---
+++ StringToListToDictionary.py
@@ -0,0 +1,11 @@
+def mydict(text):
+    print([text])
+    my_list = text.split()
+    my_dist = {}
+    i=0
+    for letter in my_list:
+        for char1 in letter:
+            print(char1,i)
+            my_dist[i]=char1
+            print(my_dist)
+            i=i+1
